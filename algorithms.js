e91f4e86ec2db19301a569ac5aeaa380c6eda347
martin <martinjdospel@gmail.com>
2022-12-06 15:20:31 +0100

    merging two lists into one sorted list

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,5 @@
+// CLIMBING STAIRS USING MEMOIZATION
+
 const climbStairs = (n, memo = {}) => {
   if (n <= 2) return n;
   if (memo[n] !== undefined) {
@@ -8,18 +10,18 @@ const climbStairs = (n, memo = {}) => {
   return res;
 };
 
-console.log(climbStairs(0)); //1
-console.log(climbStairs(1)); //1
-console.log(climbStairs(2)); //2
-console.log(climbStairs(3)); //3
-console.log(climbStairs(4)); //5
-console.log(climbStairs(5)); //8
-console.log(climbStairs(6)); //13
-console.log(climbStairs(7)); //21
-console.log(climbStairs(21)); //17711
-console.log(climbStairs(50)); //20365011074
+// console.log(climbStairs(0)); //1
+// console.log(climbStairs(1)); //1
+// console.log(climbStairs(2)); //2
+// console.log(climbStairs(3)); //3
+// console.log(climbStairs(4)); //5
+// console.log(climbStairs(5)); //8
+// console.log(climbStairs(6)); //13
+// console.log(climbStairs(7)); //21
+// console.log(climbStairs(21)); //17711
+// console.log(climbStairs(50)); //20365011074
 
-//another solution with less code
+//another solution using post decrement
 // const climbStairs = n => {
 //     let a = 0,
 //       b = 1;
@@ -29,3 +31,21 @@ console.log(climbStairs(50)); //20365011074
 //     }
 //     return b;
 //   };
+
+// MERGING LISTS INTO SORTED LISTS
+
+function mergeTwoLists(l1, l2) {
+  if (l1 === null) {
+    return l2;
+  }
+  if (l2 === null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
